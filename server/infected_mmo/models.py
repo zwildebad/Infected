d4f52d248024650d4da9ca14ac176da38f91625c
Fallasea <canisaries@gmail.com>
2019-02-14 14:42:44 -0500

    Added gameClasses file containing all classes to be used by each game, as well as the game class itself.

--- server/infected_mmo/models.py
+++ server/infected_mmo/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 
 class User(models.Model):
+    # creating the User model, which is a table in the SQLite database
+    # each variable below is a column in the database
     user_id = models.AutoField(primary_key=True, unique=True, null=False)
     username = models.CharField(max_length=60, unique=True)
     wins = models.IntegerField(default=0, null=False)
