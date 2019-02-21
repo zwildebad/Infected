61e2fd5d4dd4ebda1051e75ae0515a5a41f1914b
Fallasea <canisaries@gmail.com>
2019-02-21 13:31:42 -0500

    Updated classes, test file to test classes. BTW love u zack

--- backend/gameClasses.py
+++ backend/gameClasses.py
@@ -1,23 +1,25 @@
 class User:  # this is the healthy player class
+    username = ""
     health = 100
-    infected = False
+    dead = False  # if user object has dead = True, create a new object of class infected for that user
     speed = 1
 
-    def is_infected(self):
+    def is_infected(self):  # if user object was damaged, they are infected
         if self.health <= 0:
-            self.infected = True
+            self.dead = True
 
-    def got_hit(self):
+    def got_hit(self):  # alter user object state after taking damage
         self.health -= 100
         User.is_infected(self)
 
 
 class Infected:  # this is the infected player class
+    username = ""
     health = 100
     speed = 1.2
-    dead = False
+    dead = False  # if infected object has dead = true, player must respawn
 
-    def is_dead(self):
+    def is_dead(self):  # same as is_infected, except already infected so must respawn instead
         if self.health <= 0:
             self.dead = True
 
@@ -27,15 +29,11 @@ class Infected:  # this is the infected player class
 
 
 class Game:  # this is a game class
-    player_count = 0
+    player_count = 0  # number players in the game overall
     infected_players = 1
     healthy_players = player_count - 1
-
-    def login(self):
-        if player_count == 50:
-            print("Please wait for someone to rage quit.")
-        else:
-            self.player_count += 1
-
-    def logout(self):
-        self.player_count -= 1
+    game_over = False  # true when winner is established
+    infected_win = False
+    players_win = False
+    time = 180  # length of game/match
+    allow_join = 120  # time before game starts after first player joins
--- tests/TestGameClasses.py
+++ tests/TestGameClasses.py
@@ -5,7 +5,6 @@ from backend import gameClasses
 class TestUserClass(unittest.TestCase):
     def test_infected(self):
         big_zomb = gameClasses.Infected()
-        self.assertTrue(big_zomb.damage, 100)
         self.assertFalse(big_zomb.dead)
         for num in range(0, 5):
             big_zomb.got_hit()
@@ -14,10 +13,15 @@ class TestUserClass(unittest.TestCase):
     def test_user(self):
         big_al = gameClasses.User()
         self.assertTrue(big_al.health, 100)
-        self.assertFalse(big_al.infected)
-        self.assertEqual(big_al.damage, 25)
+        self.assertFalse(big_al.dead)
         big_al.got_hit()
-        self.assertTrue(big_al.infected)
+        self.assertTrue(big_al.dead)
+
+    def test_game(self):
+        big_game = gameClasses.Game()
+        self.assertEqual(big_game.player_count, 0)
+        self.assertEqual(big_game.infected_players, 1)
+        self.assertFalse(big_game.game_over)
 
 
 if __name__ == '__main__':
