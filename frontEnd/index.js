d83d4cbe188e4c6cb6d22f1b833bca3d2e20a3bb
zwildebad <zwildebad@gmail.com>
2019-02-28 14:01:06 -0500

    added images

--- frontEnd/index.js
+++ frontEnd/index.js
@@ -1,8 +1,22 @@
+function initGame(){
+    var config = {
+        type: Phaser.AUTO,
+        width: window.innerWidth * .98,
+        height: window.innerHeight * .98,
+        scene: {
+            preload: preload,
+            create: create
+        }
+    };
+    var game = new Phaser.Game(config);
+    preload()
+    create()
+}
 function preload ()
 {
     this.load.setBaseURL('http://labs.phaser.io');
 
-    this.load.image('sky', 'assets/skies/space3.png');
+    this.load.image('background1', 'img/background1.png');
     this.load.image('logo', 'assets/sprites/phaser3-logo.png');
     this.load.image('red', 'assets/particles/red.png');
 }
