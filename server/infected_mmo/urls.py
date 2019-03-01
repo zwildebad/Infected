eb5e4fc3802dad18e1a56130de9ec003dbe6b13f
Fallasea <canisaries@gmail.com>
2019-03-01 13:43:27 -0500

    Edited paths.

--- server/infected_mmo/urls.py
+++ server/infected_mmo/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name='index'),
+    path('', views.index, name='index'),
     path('login', views.game_login, name='login'),
     path('logout', views.game_logout, name='logout'),
     path('settings', views.settings, name='settings')
--- server/infected_mmo/views.py
+++ server/infected_mmo/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.template.loader import get_template
-from backend import gameClasses
-from backend import main
+from .backend import gameClasses
+from .backend import main
 
-# THIS IS THE CONTROLLER IN MVC
+# THIS IS THE MODEL IN MVC
 game_list = []
 
 
@@ -17,12 +17,12 @@ def game_login(request):
     for i in range(0, len(game_list)):
         current_game = game_list[i]
         if main.login(current_game):
-            return render_to_response()  # return the current game instance
+            return render_to_response('game.html')  # return the current game instance
         else:
             new_game = gameClasses.Game()
             game_list.append(new_game)
             main.login(new_game)
-            return render_to_response() # return new game instance
+            return render_to_response('game.html')  # return new game instance
 
 
 def game_logout(request, game):
