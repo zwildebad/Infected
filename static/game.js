780acffb185faf5e86a97052d1c81b49f109a15a
Fallasea <canisaries@gmail.com>
2019-03-25 08:04:51 -0400

    Merge branch 'working' of C:\Users\Alex\IdeaProjects\Infected with conflicts.

--- static/game.js
+++ static/game.js
@@ -1,4 +1,4 @@
-function initGame(){
+function initGame() {
     var config = {
         type: Phaser.AUTO,
         width: window.innerWidth,
@@ -12,30 +12,29 @@ function initGame(){
     var human;
     var zombie;
     var background;
+    var exitButton;
     preload();
     create();
-    update()
-}
-function preload ()
-{
-    this.load.image('background1', 'assets\\backgrounds\\background1.png');
-    this.load.image('human', 'templates \\ assets\\sprites\\human.png');
-    this.load.image('zombie', 'templates \\ assets\\sprites\\zombie.png');
-    this.load.image('exitButton', 'templates \\ assets\\ui\\ExitButton.png');  //TODO onclick send ajax request to server with {"game_id": game_id}
-}
-function create ()
-{
-    background = this.add.image(window.innerWidth / 2, window.innerHeight / 2, 'background1');
-    background.setDisplaySize(window.innerWidth, window.innerHeight);
-    human = this.add.sprite(window.innerWidth / 2, window.innerHeight / 2, 'human');
-    zombie = this.add.sprite(window.innerWidth / 4, window.innerHeight / 2, 'zombie');
-    exitButton = this.add.sprite(60, 50, 'exitButton');
-    exitButton.setDisplaySize(window.innerWidth / 16, window.innerHeight / 10);
-    human.angle = 270;
-    zombie.angle = 270;
-    exitButton.inputEnabled = true;
-    exitButton.events.onInputDown.add(listener, this);
-}
-function listener(){
-    console.log("hello")
+    update();
+
+
+    function preload() {
+        this.load.image('background1', 'backgrounds\\background1.png');
+        this.load.image('human', 'assets\\sprites\\human.png');
+        this.load.image('zombie', 'assets\\sprites\\zombie.png');
+        this.load.image('exitButton', 'assets\\ui\\ExitButton.png');  //TODO onclick send ajax request to server with {"game_id": game_id}
+    }
+
+
+    function create() {
+        background = this.add.image(window.innerWidth / 2, window.innerHeight / 2, 'background1');
+        background.setDisplaySize(window.innerWidth, window.innerHeight);
+        human = this.add.sprite(window.innerWidth / 2, window.innerHeight / 2, 'human');
+        zombie = this.add.sprite(window.innerWidth / 4, window.innerHeight / 2, 'zombie');
+        exitButton = this.add.sprite(60, 50, 'exitButton');
+        exitButton.setDisplaySize(window.innerWidth / 16, window.innerHeight / 10);
+        human.angle = 270;
+        zombie.angle = 270;
+        exitButton.inputEnabled = true;
+    }
 }
